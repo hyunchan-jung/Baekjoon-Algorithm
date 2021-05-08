4074116e6be29dd17c3a72720be0c0658b869925
hyunchan123 <tmzkdlqmfn1@naver.com>
2021-05-08 11:45:21 +0900

    윤년

---
+++ 2753.py
@@ -0,0 +1,7 @@
+# -*- coding: cp949 -*-
+# À±³â
+year = int(input())
+if year >= 1 and year <= 4000:
+    if year % 4 == 0 and (year % 400 == 0 or year % 100 !=0):
+        print(1)
+    else: print(0)
