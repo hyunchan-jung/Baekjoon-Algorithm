1dbd9074a26059205adeac3e9d758024b38d9b2a
hyunchan123 <tmzkdlqmfn1@naver.com>
2021-05-22 12:33:53 +0900

    평균

--- 1546.py
+++ 1546.py
@@ -1,11 +1,11 @@
 N = int(input())
 M = input().split()
-Max = max(M)
-K = []
+MM = []
+[MM.append(int(i)) for i in M]
+Max = max(MM)
 A = 0
 for i in range(len(M)):
     M[i] = int(M[i])
     M[i] = M[i]/int(Max)*100
-    K.append(M[i])
     A += M[i]
 print(A/len(M))
