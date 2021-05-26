50b9d0410cf29f02c4f6ee46d8b45a1d0c14b6aa
hyunchan123 <tmzkdlqmfn1@naver.com>
2021-05-26 23:53:53 +0900

    알파벳 찾기

---
+++ 10809.py
@@ -0,0 +1,9 @@
+S = input()
+res = []
+alp = 'abcdefghijklmnopqrstuvwxyz'
+for i in alp:
+    try:
+        res.append(S.index(i))
+    except ValueError:
+        res.append('-1')
+[print(int(i),end=' ') for i in res]
